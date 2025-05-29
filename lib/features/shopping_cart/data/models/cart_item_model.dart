05d9af6218bd798af0765a9e0f8ebcb4611109ba
Pavel <pgriskevic75@gmail.com>
2025-05-29 11:36:19 +0300

    fix android issue

--- lib/features/shopping_cart/data/models/cart_item_model.dart
+++ lib/features/shopping_cart/data/models/cart_item_model.dart
@@ -15,16 +15,33 @@ class CartItemModel {
     return {
       'title': product.title,
       'description': product.description,
-      'images': product.images,
-      'price': product.price,
+      'images': product.images.join(','),
+      'price': product.price.toString(),
       'thumbnail': product.thumbnail,
       'quantity': quantity,
     };
   }
 
   factory CartItemModel.fromJson(Map<String, dynamic> json) {
+    // Handle images that could be either String or Uint8Array
+    List<String> imagesList;
+    if (json['images'] is String) {
+      imagesList = (json['images'] as String).split(',');
+    } else if (json['images'] is List) {
+      imagesList = (json['images'] as List).map((e) => e.toString()).toList();
+    } else {
+      // If it's a Uint8Array or other format, convert to string first
+      imagesList = [json['images'].toString()];
+    }
+
     return CartItemModel(
-      product: ProductModel.fromJson(json),
+      product: ProductModel(
+        title: json['title'] as String,
+        description: json['description'] as String,
+        images: imagesList,
+        price: (json['price'] as num).toDouble(),
+        thumbnail: json['thumbnail'] as String,
+      ),
       quantity: json['quantity'] as int? ?? 1,
     );
   }
